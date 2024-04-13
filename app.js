5b0c1979a5672f85a7d15e2cbc8cc31d63eeb496
Jose Miguel <xemiruiz@hotmail.com>
2024-04-13 14:02:11 +0200

    a

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ const ctx = canvas.getContext('2d');
 const detectionCanvas = document.createElement('canvas');
 const detectionCtx = detectionCanvas.getContext('2d');
 
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
 detectionCanvas.width = window.innerWidth;
 detectionCanvas.height = window.innerHeight;
 
-// Configura las rutas a las imágenes principales y de superposición para cada paso
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
 const steps = [
     {
         mainImage: 'imagen1.png',
@@ -44,7 +44,6 @@ const steps = [
 
 let currentStep = 0;
 
-// Función para cargar una imagen
 function loadImage(src) {
     return new Promise((resolve, reject) => {
         const img = new Image();
@@ -54,25 +53,22 @@ function loadImage(src) {
     });
 }
 
-// Función para dibujar el paso actual
 async function drawStep(stepIndex) {
     const step = steps[stepIndex];
     const mainImage = await loadImage(step.mainImage);
     const overlayImage = await loadImage(step.overlayImage);
     const detectionImage = await loadImage(step.detectionImage);
 
-    // Calcular el factor de escala para mantener la proporción
     const scaleFactor = Math.min(canvas.width / mainImage.width, canvas.height / mainImage.height);
     const scaledWidth = mainImage.width * scaleFactor;
     const scaledHeight = mainImage.height * scaleFactor;
 
-    // Calcular el centro para las imágenes escaladas
     const centerX = (canvas.width - scaledWidth) / 2;
     const centerY = (canvas.height - scaledHeight) / 2;
 
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.drawImage(mainImage, centerX, centerY, scaledWidth, scaledHeight);
-    ctx.globalAlpha = 0.0; // Opacidad cero para la imagen de superposición
+    ctx.globalAlpha = 0.0;
     ctx.drawImage(overlayImage, centerX, centerY, scaledWidth, scaledHeight);
     ctx.globalAlpha = 1.0;
 
@@ -81,18 +77,19 @@ async function drawStep(stepIndex) {
 }
 
 function handleInteraction(event) {
-    event.preventDefault(); // Prevenir acciones por defecto
     let x, y;
 
-    if (event.type.includes('touch')) {
+    if (event.type === 'touchstart') {
         x = event.touches[0].clientX - canvas.offsetLeft;
         y = event.touches[0].clientY - canvas.offsetTop;
-    } else {
+        // event.preventDefault(); // Comentado para permitir zoom y otros comportamientos predeterminados
+    } else { // Evento de clic
         x = event.clientX - canvas.offsetLeft;
         y = event.clientY - canvas.offsetTop;
     }
 
     const pixelData = detectionCtx.getImageData(x, y, 1, 1).data;
+
     console.log(`Color en el punto de interacción: R=${pixelData[0]}, G=${pixelData[1]}, B=${pixelData[2]}, A=${pixelData[3]}`);
 
     if (pixelData[0] > 100 && pixelData[1] < 50 && pixelData[2] < 50) {
@@ -104,16 +101,14 @@ function handleInteraction(event) {
 canvas.addEventListener('click', handleInteraction);
 canvas.addEventListener('touchstart', handleInteraction);
 
-// Inicia mostrando el primer paso
 drawStep(currentStep);
 
-// Función para ajustar el tamaño del canvas cuando cambia el tamaño de la ventana
 function resizeCanvas() {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
     detectionCanvas.width = window.innerWidth;
     detectionCanvas.height = window.innerHeight;
-    drawStep(currentStep); // Redibujar el contenido del canvas
+    drawStep(currentStep);
 }
 
 window.addEventListener('resize', resizeCanvas);
